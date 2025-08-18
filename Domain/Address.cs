edb480d8c7987d931585ff22c2b7fb999c81e7d1
Isat <isatalantbek@outlook.com>
2025-08-18 11:52:01 +0600

    Сделал другие проекты, а так-же сделал агрегат User

---
+++ Domain/Address.cs
@@ -0,0 +1,3 @@
+﻿namespace Domain;
+
+public record class Address(string City);
---
+++ Domain/User.cs
@@ -0,0 +1,31 @@
+﻿namespace Domain;
+
+public class User
+{
+    public Guid Id { get; init; }
+    public string Name { get; private set; }
+    public Address? Address { get; private set; }
+
+    public User(Guid id, string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentNullException(name);
+
+        Id = id;
+        Name = name;
+    }
+
+    public void SetName(string name)
+    {
+
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentNullException(name);
+
+        Name = name;
+    }
+
+    public void SetAddress(Address address)
+    {
+        Address = address;
+    }
+}

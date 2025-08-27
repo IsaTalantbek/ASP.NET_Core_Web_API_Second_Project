3656c02e39f583958a02795277e3b0c40632620c
Isat <isatalantbek@outlook.com>
2025-08-27 10:49:38 +0600

    Поменял логгер на Serilog

--- src/API/Controllers/ErrorController.cs
+++ src/API/Controllers/ErrorController.cs
@@ -3,9 +3,10 @@
 namespace API.Controllers;
 
 [ApiController]
+[ApiExplorerSettings(IgnoreApi = true)] // Это исключит контроллер из документации Swagger
+[Route("api/error")]
 public class ErrorController : ControllerBase
 {
-    [Route("api/error")]
     public IActionResult HandleError()
     {
         return StatusCode(500, new ErrorResponse("InternalError", "unkown internal error", new {}));
